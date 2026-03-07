6c447b3cb8aac4371d9f9fb041eff5c19d4c2981
vexislinki57 <62660394+leandrogallo-dev@users.noreply.github.com>
2026-03-07 20:31:53 -0300

    Create init_database.sql to set up DataWarehouse
    This script initializes the DataWarehouse database and creates the bronze, silver, and gold schemas.

---
+++ scripts/init_database.sql
@@ -0,0 +1,31 @@
+/*
+=========================================
+Create Datbase and Schemas
+=========================================
+Script Purpose:
+	...
+WARNING:
+	...
+*/
+
+
+USE master;
+GO
+
+IF EXISTS (SELECT 1 FROM sys.database WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+CREATE DATABASE DataWarehouse;
+GO
+
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
